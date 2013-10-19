d1ac31045b06c2760de605191ed8e3a4b811fde7
issei <issei.m7@gmail.com>
2013-10-20 04:17:03 +0900

    supported sfDependencyInjectionPlugin v1.1

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,5 +1,9 @@
 <?php
 
+// Composer autoload
+require_once dirname(__DIR__).'/plugins/autoload.php';
+
+// symfony1 autoload
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
